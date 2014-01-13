126c72836bf5627c93a6799c83f8d0132237958d
rsinghal <rashmi.s.singhal@gmail.com>
2014-01-13 12:28:43 -0500

    differentiate between mets and mods. placeholders for mods code

--- manifests/urls.py
+++ manifests/urls.py
@@ -3,8 +3,12 @@ from django.conf.urls import patterns, url
 from manifests import views
 
 urlpatterns = patterns('',
-    url(r'^(?P<document_id>\d+)$', views.manifest, name='manifest'),
-    url(r'^delete/(?P<document_id>\d+)$', views.delete, name='delete'),
-    url(r'^refresh/(?P<document_id>\d+)$', views.refresh, name='refresh'),
+    url(r'^mets/(?P<document_id>[A-Za-z\d]+)$', views.manifest_mets, name='manifest_mets'),
+    url(r'^mods/(?P<document_id>[A-Za-z\d]+)$', views.manifest_mods, name='manifest_mods'),
+    url(r'^delete/(?P<document_id>[A-Za-z\d]+)$', views.delete, name='delete'),
+    url(r'^refresh/mets/(?P<document_id>[A-Za-z\d]+)$', views.refresh_mets, name='refresh_mets'),
+    url(r'^refresh/mods/(?P<document_id>[A-Za-z\d]+)$', views.refresh_mods, name='refresh_mods'),
+    url(r'^refresh/mets/all$', views.refresh_mets_all, name='refresh_mets_all'),
+    url(r'^refresh/mods/all$', views.refresh_mods_all, name='refresh_mods_all'),
     url(r'^refresh/all$', views.refresh_all, name='refresh_all')
 )
--- manifests/views.py
+++ manifests/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from manifests import hc
+from manifests import mets
+from manifests import mods
 from manifests import models
 import json
 import urllib2
 
 # Create your views here.
 
-DRS_URL = "http://fds.lib.harvard.edu/fds/deliver/"
+METS_DRS_URL = "http://fds.lib.harvard.edu/fds/deliver/"
+MODS_DRS_URL = "http://webservices.lib.harvard.edu/rest/MODS/via/"
 
-def manifest(request, document_id):
+# Returns a IIIF manifest of a METS document in the DRS
+# Checks if DB has it, otherwise creates it
+def manifest_mets(request, document_id):
     # Check if manifest exists
     has_manifest = models.manifest_exists(document_id)
 
@@ -23,18 +27,23 @@ def manifest(request, document_id):
             return mets # This is actually the 404 HttpResponse, so return and end the function
  
         # Convert to shared canvas model if successful
-        converted_json = hc.main(mets, document_id)
+        converted_json = mets.main(mets, document_id)
         # Store to elasticsearch
         models.add_or_update_manifest(document_id, converted_json)
         
-        response = HttpResponse(converted_json, content_type="application/json")
+        response = HttpResponse(converted_json)
     else:
         # return JSON from db
         json_doc = models.get_manifest(document_id)
-        response = HttpResponse(json.dumps(json_doc), content_type="application/json")
+        response = HttpResponse(json.dumps(json_doc))
     
-    response["Access-Control-Allow-Origin"] = "*"
+    add_headers(response)
     return response
+
+# Returns a IIIF manifest of a MODS document in the DRS
+# Checks if DB has it, otherwise creates it
+def manifest_mods(request, document_id):
+    pass
 
 def delete(request, document_id):
     # Check if manifest exists
@@ -48,26 +57,31 @@ def delete(request, document_id):
 
 # Force refresh a single document
 # Pull METS, rerun conversion script, and store in db
-def refresh(request, document_id):
+def refresh_mets(request, document_id):
     (success, mets) = get_mets(document_id)
 
     if not success:
         return mets # This is actually the 404 HttpResponse, so return and end the function
 
     # Convert to shared canvas model if successful
-    converted_json = hc.main(mets, document_id)
+    converted_json = mets.main(mets, document_id)
 
     # Store to elasticsearch
     models.add_or_update_manifest(document_id, converted_json)
 
-    response = HttpResponse(converted_json, content_type="application/json")
-    response["Access-Control-Allow-Origin"] = "*"
+    response = HttpResponse(converted_json)
+    add_headers(response)
     return response
 
-# Force refresh all document in the db
-# Might need to tweak so not hitting DRS too frequently 
-# and bulk load to elasticsearch
-def refresh_all(request):
+# Refresh a single document
+# Pull MODS, rerun conversion script, store in db
+def refresh_mods(request, document_id):
+    pass
+
+# Force refresh all originally METS documents in the db
+# Might need to tweak so not hitting DRS as frequently 
+# TODO: bulk load to elasticsearch
+def refresh_mets_all(request):
     count = 0
     ids = models.get_all_manifest_ids()
     for document_id in ids:
@@ -75,14 +89,25 @@ def refresh_all(request):
         if not success:
             continue # don't need to keep processing because it doesn't exist in DRS
         count = count + 1
-        converted_json = hc.main(mets, document_id)
+        converted_json = mets.main(mets, document_id)
         models.add_or_update_manifest(document_id, converted_json)
 
     return HttpResponse("Successfully refreshed %s of %s documents" % (count, len(ids)))
 
+# Force refresh all originally MODS documents in the db
+# Might need to tweak so not hitting DRS as frequently
+# TODO: bulk load to elasticsesarch
+def refresh_mods_all(request):
+    pass
+
+# Force refresh all documents in db (originally METS and MODS)
+# Same issues as other refresh functions
+def refresh_all(request):
+    pass
+
 ## HELPER FUNCTIONS ##
 def get_mets(document_id):
-    mets_url = DRS_URL+document_id
+    mets_url = METS_DRS_URL+document_id
     try:
         response = urllib2.urlopen(mets_url)
     except urllib2.HTTPError, err:
@@ -92,3 +117,20 @@ def get_mets(document_id):
 
     mets = response.read()
     return (True, mets)
+
+def get_mods(document_id):
+    mods_url = MODS_DRS_URL+document_id
+    try:
+        response = urllib2.urlopen(mods_url)
+    except urllib2.HTTPError, err:
+        if err.code == 500:
+            # document does not exist in DRS
+            return (False, HttpResponse("The document ID %s does not exist" % document_id, status=404))
+
+    mods = response.read()
+    return (True, mods)
+
+def add_headers(response):
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Content-Type"] = "application/ld+json"
+    return response
