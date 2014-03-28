ef21297c23f392aa0c095efd47b80e9fd57c2959
rsinghal <rashmi.s.singhal@gmail.com>
2014-03-28 17:51:08 -0400

    create TOC using ranges.  hollis check fix

--- manifests/mets.py
+++ manifests/mets.py
@@ -12,6 +12,7 @@ hulDrsAdminNS ="http://hul.harvard.edu/ois/xml/ns/hulDrsAdmin"
 
 ALLNS = {'mets':metsNS, 'mods':modsNS, 'xlink':xlinkNS, 'premis':premisNS, 'hulDrsAdmin':hulDrsAdminNS}
 imageHash = {}
+rangesJsonList = []
 
 ## TODO: Other image servers?
 imageUriBase = "http://ids.lib.harvard.edu/ids/iiif/"
@@ -25,19 +26,56 @@ HOLLIS_URL = "http://webservices.lib.harvard.edu/rest/MODS/hollis/"
  ## Add ISO639-2B language codes here where books are printed right-to-left (not just the language is read that way)
 right_to_left_langs = set(['ara','heb'])
 
-def process_struct_map(st, canvasInfo):
+def process_struct_map(st, canvasInfo, ranges):
 	if 'LABEL' in st.attrib:
 		label = st.xpath('./@LABEL')[0]
 	else:
 		label = st.xpath('./@ORDER')[0]
-
 	for fid in st.xpath('.//mets:fptr/@FILEID', namespaces=ALLNS):
 		info = {}
 		info['label'] = label
 		if fid in imageHash.keys():
 			info['image'] = imageHash[fid]
 			canvasInfo.append(info)
+			range = {}
+			range[label] = imageHash[fid]
+			ranges.append(range)
+	return ranges
 
+def create_range_json(ranges, manifest_uri, range_id, within, label):
+	# this is either a list or the image id in the METS
+	if type(ranges) is list:
+		canvases = []
+	else:
+		canvases = [manifest_uri + "/canvas/canvas-%s.json" % ranges]
+
+	rangejson =  {"@id": range_id,
+		      "@type": "sc:Range",
+		      "label": label,
+		      "canvases": canvases
+		      }
+	# top level "within" equals the manifest_uri, so this range is a top level
+	if within != manifest_uri:
+		rangejson["within"] = within
+	rangesJsonList.append(rangejson)
+
+def create_ranges(ranges, previous_id, manifest_uri):
+	if not type(ranges) is list:
+		return
+	counter = 0
+	for ri in ranges:
+		counter = counter + 1
+		label = ri.keys()[0]
+		if previous_id == manifest_uri:
+			# these are for the top level divs
+			range_id = manifest_uri + "/range/range-%s.json" % counter
+		else:
+			# otherwise, append the counter to the parent's id
+			range_id = previous_id[0:previous_id.rfind('.json')] + "-%s.json" % counter
+		new_ranges = ri.get(label)
+		create_range_json(new_ranges, manifest_uri, range_id, previous_id, label)
+		create_ranges(new_ranges, range_id, manifest_uri)
+	
 def main(data, document_id, source):
 	dom = etree.XML(data)
 	# Check if this is a DRS2 object since some things, like hollis ID are in a different location
@@ -60,8 +98,10 @@ def main(data, document_id, source):
 	viewingDirection = 'left-to-right' # default
 	if isDrs1:
 		hollisCheck = dom.xpath('/mets:mets/mets:dmdSec/mets:mdWrap/mets:xmlData/mods:mods/mods:identifier[@type="hollis"]/text()', namespaces=ALLNS)
+		hollisCheck = []
 	else:
 		hollisCheck = dom.xpath('/mets:mets/mets:amdSec//hulDrsAdmin:hulDrsAdmin/hulDrsAdmin:drsObject/hulDrsAdmin:harvardMetadataLinks/hulDrsAdmin:metadataIdentifier[../hulDrsAdmin:metadataType/text()="Aleph"]/text()', namespaces=ALLNS)
+		hollisCheck = []
 	if len(hollisCheck) > 0:
 		hollisID = hollisCheck[0].strip()
 		response = urllib2.urlopen(HOLLIS_URL+hollisID).read()
@@ -89,23 +129,32 @@ def main(data, document_id, source):
 
 	#print imageHash
 	canvasInfo = []
+	rangeInfo = []
 	for st in struct:
-		subdivs = st.xpath('./mets:div[@LABEL]', namespaces = ALLNS)
-
+		subdivs = st.xpath('./mets:div[@LABEL]', namespaces = ALLNS) 
+		if 'LABEL' in st.attrib:
+			rangeKey = st.xpath('./@LABEL')[0]
+		else:
+			rangeKey = st.xpath('./@ORDER')[0]
 		# need to be able to handle any number of nesting
 		if len(subdivs) > 0:
+			rangesForKey = []
 			# need to process subdivs because object has nesting
 			# there should be a max of 3 levels, but will need more testing
 			for sd in subdivs:
 				subdivs2 = sd.xpath('./mets:div[@LABEL]', namespaces = ALLNS)
+				rangeKey2 = sd.xpath('./@LABEL')[0]
 				if len(subdivs2) > 0:
+					rangesForKey2 = []
 					for sd2 in subdivs2:
-						process_struct_map(sd2, canvasInfo)
+						rangesForKey2 = process_struct_map(sd2, canvasInfo, rangesForKey2)
+					rangesForKey.append({rangeKey2 : rangesForKey2})
 				else:
-					process_struct_map(sd, canvasInfo)
+					rangesForKey = process_struct_map(sd, canvasInfo, rangesForKey)
+			rangeInfo.append({rangeKey : rangesForKey})
 		else:
-			process_struct_map(st, canvasInfo)	
-
+			rangesForKey = process_struct_map(st, canvasInfo, rangeInfo)
+#	print rangeInfo
 	mfjson = {
 		"@context":"http://www.shared-canvas.org/ns/context.json",
 		"@id": manifest_uri,
@@ -119,7 +168,8 @@ def main(data, document_id, source):
 				"@id": manifest_uri + "/sequence/normal.json",
 				"@type": "sc:Sequence",
 			}
-		]
+		],
+		"structures": []
 	}
 
 	canvases = []
@@ -149,29 +199,11 @@ def main(data, document_id, source):
 		}
 		canvases.append(cvsjson)
 
-	## TODO
-	rangeInfo = []
 	# build table of contents using Range and Structures
-	for st in struct:
-		if 'LABEL' in st.attrib:
-			label = st.xpath('./@LABEL')[0]
-		else:
-			label = st.xpath('./@ORDER')[0]
-
-		#print "Label: %s" % label
-		subdivs = st.xpath('./mets:div[@LABEL]', namespaces = ALLNS)
-		while len(subdivs) > 0:
-			for sd in subdivs:
-				label = sd.xpath('./@LABEL')[0]
-				#print "Label: %s" % label
-				parent = sd.xpath('../mets:div[@LABEL]', namespaces = ALLNS)
-				if len(parent) == 1:
-					within = parent.xpath('./@LABEL')[0]
-					#print "%s within %s" % (label, within)
-				subdivs = sd.xpath('./mets:div[@LABEL]', namespaces = ALLNS)
+	create_ranges(rangeInfo, manifest_uri, manifest_uri)
 
 	mfjson['sequences'][0]['canvases'] = canvases
-	#mfjson['structures'][0] = ranges
+	mfjson['structures'] = rangesJsonList
 
 	output = json.dumps(mfjson, indent=4, sort_keys=True)
 	return output
