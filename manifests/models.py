08832446f3977eb45a442cf859c35a5919bf1957
rsinghal <rashmi.s.singhal@gmail.com>
2015-12-03 15:03:38 -0500

    don't get a new connection every time

--- manifests/models.py
+++ manifests/models.py
@@ -6,6 +6,7 @@ from elasticsearch import Elasticsearch
 
 ELASTICSEARCH_URL = getattr(settings, 'ELASTICSEARCH_URL', 'localhost:9200')
 ELASTICSEARCH_INDEX = getattr(settings, 'ELASTICSEARCH_INDEX', 'manifests')
+es = Elasticsearch(ELASTICSEARCH_URL)
 
 # Connect to elasticsearch db
 def get_connection():
@@ -13,27 +14,22 @@ def get_connection():
 
 # Gets the content of a manifest, returns JSON
 def get_manifest(manifest_id, source):
-    es = get_connection()
     return es.get(index=ELASTICSEARCH_INDEX, doc_type=source, id=manifest_id)["_source"]
 
 # Inserts JSON document into elasticsearch with the given manifest_id
 # Either adds new document or replaces existing document
 def add_or_update_manifest(manifest_id, document, source):
-    es = get_connection()
     es.index(index=ELASTICSEARCH_INDEX, doc_type=source, id=manifest_id, body=document)
 
 # Deletes manifest from elasticsearch (need to refresh index?)
 def delete_manifest(manifest_id, source):
-    es = get_connection()
     es.delete(index=ELASTICSEARCH_INDEX, doc_type=source, id=manifest_id)
 
 # Checks if manifest exists in elasticsearch, returns boolean
 def manifest_exists(manifest_id, source):
-    es = get_connection()
     return es.exists(index=ELASTICSEARCH_INDEX, doc_type=source, id=manifest_id)
 
 def get_all_manifest_ids_with_type(source):
-    es = get_connection()
     results = es.search(index="manifests", doc_type=source, fields="[]")
     ids = []
     for r in results["hits"]["hits"]:
@@ -41,7 +37,6 @@ def get_all_manifest_ids_with_type(source):
     return ids
 
 def get_all_manifest_ids():
-    es = get_connection()
     results = es.search(index="manifests", fields="[]")
     ids = []
     for r in results["hits"]["hits"]:
@@ -49,5 +44,4 @@ def get_all_manifest_ids():
     return ids
 
 def get_manifest_title(manifest_id, source):
-    es = get_connection()
     return es.get(index=ELASTICSEARCH_INDEX, doc_type=source, id=manifest_id)["_source"]["label"]
